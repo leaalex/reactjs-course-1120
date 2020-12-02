783249340dfe8acfba128b0e30fa2188d2a08aba
leaalex <lea_alex@mail.ru>
2020-12-02 11:36:53 +0500

    day-2-start

--- day-2/src/App.js
+++ day-2/src/App.js
@@ -2,18 +2,56 @@ import React, {Component} from "react";
 import './App.css';
 
 
+
 class App extends Component {
+    state = {
+        data: [
+            {id: 1, title: 'Купить хлеба', checked: true},
+            {id: 2, title: 'Помыть машину', checked: false},
+            {id: 3, title: 'Изучить ReactJS', checked: true},
+            {id: 4, title: 'Хорошо поспать', checked: false},
+        ]
+    }
+  handlerClick = (event) => {
+        console.log(event)
+  }
+
+  handlerKeyUp = (event) => {
+        if (event.code === 'Enter'){
+            const newTask = {id: 5, title: event.target.value, checked: false}
+            this.setState({data: [...this.state.data, newTask]})
+        }
+  }
+
   render() {
-    return (
-        <div className="container">
-            <div className="input-group m-3">
-                <input type="text" className="form-control" placeholder="Recipient's username"
-                       aria-label="Recipient's username" aria-describedby="button-addon2" />
-                <div className="input-group-append">
-                    <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+    const tasklist = this.state.data.map(task => (
+        <div key={task.id} className="input-group mb-3">
+            <div className="input-group-prepend">
+                <div className="input-group-text">
+                    <input type="checkbox" defaultChecked={task.checked} />
                 </div>
             </div>
+            <input type="text" className="form-control" defaultValue={task.title}/>
         </div>
+        ))
+
+
+    return (
+        <div className="container p-3">
+            <div className="input-group mb-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Новая задача"
+                    onKeyUp={this.handlerKeyUp}
+                        />
+                <div className="input-group-append">
+                    <button className="btn btn-outline-secondary" onClick={this.handlerClick}>+ Создать</button>
+                </div>
+            </div>
+            { tasklist }
+        </div>
+
     );
   }
 }
