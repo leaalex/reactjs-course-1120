4783049dfaa9ceab75868171f1acd3042de2173b
leaalex <lea_alex@mail.ru>
2020-12-02 12:02:56 +0500

    day-2-start

--- day-2/src/App.js
+++ day-2/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import './App.css';
 
-
+const inputText = React.createRef()
 
 class App extends Component {
     state = {
@@ -10,20 +10,29 @@ class App extends Component {
             {id: 2, title: 'Помыть машину', checked: false},
             {id: 3, title: 'Изучить ReactJS', checked: true},
             {id: 4, title: 'Хорошо поспать', checked: false},
-        ]
+        ],
     }
+
   handlerClick = (event) => {
-        console.log(event)
+      const newTask = {id: 5, title: inputText.current.value, checked: false}
+      this.setState({data: [...this.state.data, newTask], inputText: ''})
+      inputText.current.value = ''
   }
 
   handlerKeyUp = (event) => {
         if (event.code === 'Enter'){
             const newTask = {id: 5, title: event.target.value, checked: false}
             this.setState({data: [...this.state.data, newTask]})
-        }
+            event.target.value = ''
+        } if (event.code === 'Escape') {
+          event.target.value = ''
+          event.target.blur()
+      }
   }
 
+
   render() {
+        console.log('render', this.state)
     const tasklist = this.state.data.map(task => (
         <div key={task.id} className="input-group mb-3">
             <div className="input-group-prepend">
@@ -44,6 +53,7 @@ class App extends Component {
                     className="form-control"
                     placeholder="Новая задача"
                     onKeyUp={this.handlerKeyUp}
+                    ref={inputText}
                         />
                 <div className="input-group-append">
                     <button className="btn btn-outline-secondary" onClick={this.handlerClick}>+ Создать</button>
